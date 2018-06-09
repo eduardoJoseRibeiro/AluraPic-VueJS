451f29fe7876b7c43d162cea70178088da223fcc
Eduardo Ribeiro <joseribeiroejrs@ufu.br>
2018-06-09 11:21:51 -0300

    Criando paineis

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,13 @@
     <h1>{{ titulo }}</h1>
     <ul>
       <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.descricao">
+        
+        <div class="painel">
+          <h2 class="painel-titulo">{{ foto.titulo }}</h2>
+          <div class="painel-conteudo">
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+          </div>
+        </div>
       </li>
     </ul>
   </div>
@@ -30,12 +36,14 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Helvetica, 'Avenir', Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin: 0 auto;
   margin-top: 60px;
+  width: 96%;
 }
 
 h1, h2 {
@@ -55,4 +63,31 @@ li {
 a {
   color: #42b983;
 }
+
+.imagem-responsiva{
+  width: 100%;
+}
+
+/* estilo do painel */ 
+
+   .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
 </style>
